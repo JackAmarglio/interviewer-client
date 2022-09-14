cb5b593387f0ee801efe8dcb14a0d53dd838ab5e
JackAmarglio <topdevking@gmail.com>
2022-09-14 10:15:25 +0800

    clean landing file

--- client/src/pages/landing/index.js
+++ client/src/pages/landing/index.js
@@ -1,9 +1,5 @@
-import { HeaderBar } from "../layout/HeaderBar";
 import { connect } from "react-redux";
-import LandingLogo from "../../Images/mobileGuy.svg";
 import { Grid, Typography, Box, styled, Button } from "@mui/material";
-import { useHistory } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
 import "animate.css"
 import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect } from "react";
@@ -11,8 +7,7 @@ import Welcome from "./Welcome"
 
 
 
-function LandingPage(props) {
-    const { loggedIn } = props;
+function LandingPage() {
     const [anim, setAnim] = useState(false)
     useEffect(() => {
         setTimeout(() => {
@@ -21,7 +16,6 @@ function LandingPage(props) {
     }, []);
     return (
         <Box sx={{ flexGrow: 1, overflowX: "hidden" }} margin="2% 5%">
-            {/* <HeaderBar /> */}
             {!anim &&
                 <Box className="animate__animated animate__flipInY animate__delay-1s animate__zoomOutRight">
                     <img src="logo.png" alt="" />
