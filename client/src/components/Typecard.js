fa2c63c574a43ab2debd734c8d34c9b156bab425
Veteran112 <topdevking@gmail.com>
2022-11-06 21:43:46 -0400

    update responsive design to auth part

--- client/src/components/Typecard.js
+++ client/src/components/Typecard.js
@@ -16,7 +16,7 @@ const Typecard = (props) => {
 
     return (
         <Box sx={{ mx: "30px", display: "flex", alignItems: "center", flexDirection: "column" }}>
-            <Button onClick={setUserType} sx={{ width: "200px", height: "70px", backgroundColor: "#00F000", color: "#FFFFFF", fontSize: "20px" }}>{props.type}</Button>
+            <Button onClick={setUserType} sx={{ width: "200px", height: "70px", backgroundColor: "#00F000", color: "#FFFFFF", fontSize: "20px", marginTop: '30px' }}>{props.type}</Button>
             <Box sx={{ width: "300px", height: "250px", mt: "20px", backgroundColor: "#F0F0F0", padding: "10px" }}>
                 <Typography component="h3" sx={{ mb: "20px" }}>{props.title}</Typography>
                 {props.content.map((item, index) => {
